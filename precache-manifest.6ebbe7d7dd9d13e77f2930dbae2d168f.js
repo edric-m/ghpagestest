3bb19ed7cd9efa812cf98db9029f5d05dfe68366
Erica Mendoza <edjomendoza@gmail.com>
2020-02-15 12:11:45 +1100

    Updates

---
+++ precache-manifest.6ebbe7d7dd9d13e77f2930dbae2d168f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c1dbd768bfbc068dcc118f8be73cc0",
+    "url": "/ghpagestest/index.html"
+  },
+  {
+    "revision": "bfb1c13c8c2eff62a32b",
+    "url": "/ghpagestest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8a421eb1ff5d602cb461",
+    "url": "/ghpagestest/static/js/2.be56b615.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ghpagestest/static/js/2.be56b615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb1c13c8c2eff62a32b",
+    "url": "/ghpagestest/static/js/main.64e5fab1.chunk.js"
+  },
+  {
+    "revision": "b017c7864eaa4808a442",
+    "url": "/ghpagestest/static/js/runtime-main.aa92f347.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpagestest/static/media/logo.5d5d9eef.svg"
+  }
+]);
